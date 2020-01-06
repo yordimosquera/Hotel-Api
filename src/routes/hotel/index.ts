0e4bc03f222875f9bb827b6d0f0196ef5ebff456
yordimosquera <yordi.mosquera@almundo.com>
2020-01-06 01:08:12 -0500

    the root route added

--- src/routes/hotel/index.ts
+++ src/routes/hotel/index.ts
@@ -3,6 +3,11 @@ import { Controller } from "../../controllers/hotel";
 export const Hotel = {
   find: {
     verb: "get",
+    mountPoint: "/",
+    handler: Controller.findHotels
+  },
+  findHotels: {
+    verb: "get",
     mountPoint: "/hotels",
     handler: Controller.findHotels
   },
