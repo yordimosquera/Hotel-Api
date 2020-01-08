c4c3ef0fc5714761b458d5726b043b6f03c1628e
yordimosquera <yordi.mosquera@almundo.com>
2020-01-07 20:24:52 -0500

    environment variables changed

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,28 +1,29 @@
-import express from 'express'
-import http from 'http'
+import express from "express";
+import http from "http";
 
 export abstract class InfraWeb {
-    private server: any
-    public app: any
-    constructor() {
-        this.app = express()
-    }
-    public use(middlewares: any): void {
-        Object.keys(middlewares).forEach((key) => {
-            this.app.use(middlewares[key].mountPoint, middlewares[key].handler)
-        })
-    }
+  private server: any;
+  public app: any;
+  constructor() {
+    this.app = express();
+  }
+  public use(middlewares: any): void {
+    Object.keys(middlewares).forEach(key => {
+      this.app.use(middlewares[key].mountPoint, middlewares[key].handler);
+    });
+  }
 
-    public mountRoutes(routes: any): void {
-        Object.keys(routes).forEach((key) => {
-            this.app[routes[key].verb](routes[key].mountPoint, routes[key].handler)
-        })
-    }
+  public mountRoutes(routes: any): void {
+    Object.keys(routes).forEach(key => {
+      this.app[routes[key].verb](routes[key].mountPoint, routes[key].handler);
+    });
+  }
 
-    public listen() {
-        const port: number = 3200
-        this.server = http.createServer(this.app)
-        this.server.listen(port, () => { console.log(`Server running at ${port}`) })
-    }
-
+  public listen() {
+    const port: any = process.env.PORT || 3200;
+    this.server = http.createServer(this.app);
+    this.server.listen(port, () => {
+      console.log(`Server running at ${port}`);
+    });
+  }
 }
